070517ef4a928cc6e7dcc723b80b7a9d41191543
Timofey Klyubin <klyubin@teamidea.ru>
2017-10-14 22:47:42 +0300

    fix spring

--- src/main/java/ru/teamidea/agileqa/controller/MainController.java
+++ src/main/java/ru/teamidea/agileqa/controller/MainController.java
@@ -3,13 +3,19 @@ package ru.teamidea.agileqa.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController{
 
     @RequestMapping("/hello")
     public String hello() {
+        return "index"; // this is view name
+    }
 
+    @RequestMapping("/hello-body")
+    @ResponseBody
+    public String helloBody() {
         return "<h2>HELLO FROM CLASS</h2>";
     }
 }
